c563fc9e4601dfa7189be9cdeb580050527877ca
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-06-19 12:47:54 +0000

    Added database and secret key for django and heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xf2v@mz^y%xn@v(64zt_=)$0a=++q0!am0nvj4eu@r79r-h!)-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
