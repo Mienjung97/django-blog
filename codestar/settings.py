2c8e2c70fb83b9361d259227c8d122f6376d03c6
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-06-27 14:39:23 +0000

    Bugfix

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mienjung97-djangoblog-c5sboos6uqa.ws.codeinstitute-ide.net', '.herokuapp.com']
 
